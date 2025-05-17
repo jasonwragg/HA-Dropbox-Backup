47ce74707fc7c7b80f4cd53e2e87c0153fb7934c
Jason Wragg <Jason@hatleygarage.co.uk>
2025-05-17 14:12:09 +0000

    pylint fixed for config_flow

--- custom_components/dropboxbackup/config_flow.py
+++ custom_components/dropboxbackup/config_flow.py
@@ -1,12 +1,23 @@
+"""Config flow for Dropbox integration."""
+
 import voluptuous as vol
 from homeassistant import config_entries
 from .const import DOMAIN, CONF_ACCESS_TOKEN, CONF_FOLDER
 
 
 class DropboxBackupFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Config flow for Dropbox integration."""
+
     VERSION = 1
 
+    @classmethod
+    def is_matching(cls, dict_) -> bool:
+        """Test if entry matches dict."""
+        return True
+
     async def async_step_user(self, user_input=None):
+        """Handle the initial step."""
+
         if user_input is None:
             schema = vol.Schema(
                 {
